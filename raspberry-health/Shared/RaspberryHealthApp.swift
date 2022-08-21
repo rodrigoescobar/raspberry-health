b51fe307c6b061b9c47af539dd002a4f1e252550
Rodrigo Escobar <rod@escobar.dev>
2022-08-21 15:57:16 -0700

    First commit

---
+++ raspberry-health/Shared/RaspberryHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaspberryHealthApp.swift
+//  Shared
+//
+//  Created by Rodrigo Escobar on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct RaspberryHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
